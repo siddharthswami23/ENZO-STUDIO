983f1da4cc876cb740086476f4bb95f18f974e24
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-02 14:31:53 +0530

    gsap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Header = () => {
+  useGSAP(() => {
+    gsap.to("img", {
+      rotate: 360,
+      duration: 5,
+      repeat: -1,
+      ease: "linear",
+    });
+  });
   return (
     <div className="w-full flex items-center justify-between absolute px-28 py-20 z-10">
-        <img src="https://static.wixstatic.com/media/f1c650_35a00b1fe6ce403eb237970ec8d34f79~mv2.png/v1/fill/w_99,h_99,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Frame%2053.png" alt="" />
-        <button className="bg-black text-white px-10 py-2 border-white border-solid border-[5px] rounded-full hover:scale-110 transition-colors duration-300">Hire me</button>
+      <img
+        src="https://static.wixstatic.com/media/f1c650_35a00b1fe6ce403eb237970ec8d34f79~mv2.png/v1/fill/w_99,h_99,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Frame%2053.png"
+        alt=""
+      />
+      <button className="bg-black text-white px-10 py-2 border-white border-solid border-[5px] rounded-full hover:scale-110 transition-colors duration-300">
+        Hire me
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
