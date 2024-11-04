1c4c6b3f03a1ebc7e9f33e57a005d28ca1215d8d
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-04 12:24:48 +0530

    page 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import Page3 from "./pages/Page3";
 import Page4 from "./pages/Page4";
-import Page5 from "./pages/Page5";
 const App = () => {
   return (
     <>
@@ -12,7 +11,6 @@ const App = () => {
       <Page2 />
       <Page3 />
       <Page4 />
-      <Page5 />
     </>
   );
 };
--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -1,7 +1,18 @@
 const Page4 = () => {
   return (
     <div className="w-full h-screen bg-white p-7">
-      <div className="w-full h-full bg-black rounded-[50px] flex items-center justify-center"></div>
+      <div className="w-full h-full bg-black rounded-[50px] overflow-hidden relative">
+        <h1 className="absolute font-[regular] uppercase text-[30vw] text-white tracking-wide left-10 -bottom-32">
+          About
+        </h1>
+        <video
+          loop
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+          src="https://video.wixstatic.com/video/11062b_92619c730c4b4b0bb27ea39c4276a52b/480p/mp4/file.mp4"
+        ></video>
+      </div>
     </div>
   );
 };
