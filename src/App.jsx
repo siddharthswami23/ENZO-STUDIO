eb56c67e5c43973ddab5d49dfed465ffb1ba14e9
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-04 12:43:57 +0530

    lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import Page3 from "./pages/Page3";
 import Page4 from "./pages/Page4";
 import Page5 from "./pages/Page5";
+import Lenis from "lenis";
+
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    // Listen for the scroll event and log the event data
+    lenis.on("scroll", () => {
+      // console.log(e);
+    });
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
   return (
     <>
       <Header />
