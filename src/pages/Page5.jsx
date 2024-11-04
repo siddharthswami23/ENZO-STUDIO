1f98c9f0f027267c89b415cce726e6c595415170
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-04 12:37:26 +0530

    page 5

---
+++ src/pages/Page5.jsx
@@ -0,0 +1,9 @@
+const Page5 = () => {
+  return (
+    <div className="w=full bg-cover bg-center bg-no-repeat bg-[url('https://static.wixstatic.com/media/f1c650_8b19a36c22b44b04aa056d2f4482aeac~mv2.jpeg/v1/fill/w_1192,h_917,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f1c650_8b19a36c22b44b04aa056d2f4482aeac~mv2.jpeg')]">
+        <img className="relative w-full h-[150vh] object-cover" src="https://static.wixstatic.com/media/f1c650_360814da288f418991547fa9d5e08904~mv2.png/v1/fill/w_1903,h_1464,al_c,q_95,usm_0.66_1.00_0.01,enc_auto/Subtract.png" alt="" />
+    </div>
+  )
+}
+
+export default Page5
